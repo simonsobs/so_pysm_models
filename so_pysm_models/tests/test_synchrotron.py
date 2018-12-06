2ca0d05de6aa01499c3600115ea4192b2ef3bd2b
Andrea Zonca <code@andreazonca.com>
2018-12-06 11:48:47 -0800

    add unit test for synchrotron, QU only

---
+++ so_pysm_models/tests/test_synchrotron.py
@@ -0,0 +1,19 @@
+import numpy as np
+import healpy as hp
+from astropy.utils.data import get_pkg_data_filename
+
+from .. import GaussianSynchrotron
+
+
+def test_gaussian_synchrotron():
+    test_map_filename = get_pkg_data_filename(
+        "data/Gaussian_synch_23GHz_uRJ_testmap.fits.zip"
+    )
+    test_map = hp.read_map(test_map_filename, (0, 1, 2))
+
+    nside = hp.npix2nside(len(test_map[0]))
+    gaussian_synch = GaussianSynchrotron(target_nside=nside)
+    np.random.seed(31)
+    m = gaussian_synch.signal(23)
+
+    np.testing.assert_allclose(m, test_map)
