e39379ce61cd93fe8623a85132d2915f45d55bac
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-12-20 15:45:25 -0800

    improvements to PrecomputedAlms

--- so_pysm_models/alms.py
+++ so_pysm_models/alms.py
@@ -1,17 +1,17 @@
 import numpy as np
 import healpy as hp
-try: from pixell import curvedsky
+try: from pixell import curvedsky,enmap
 except: pass
-
 import pysm
 
-class PrecomputedAlms:
+class PrecomputedAlms(object):
 
     def __init__(
-        self, filename, input_units,
+        self, filename,
             target_nside = None, 
             target_shape = None,
             target_wcs = None,
+            input_units="uK_RJ",
             has_polarization=True, pixel_indices=None
     ):
         """Generic component based on Precomputed Alms
@@ -42,30 +42,41 @@ class PrecomputedAlms:
         self.pixel_indices = pixel_indices
         self.has_polarization = has_polarization
 
-        self.alm = np.complex128(
+        alm = np.complex128(
             hp.read_alm(self.filename, hdu=(1, 2, 3) if self.has_polarization else 1)
         )
 
-        # use tile to output the same map for all frequencies
         if self.nside is None:
+            assert (self.shape is not None) and (self.wcs is not None)
             ncomp = 3 if self.has_polarization else 1
             self.omap = enmap.empty( (ncomp,)+self.shape[-2:], self.wcs)
-            curvedsky.alm2map(self.alm, self.omap, spin = [0, 2], verbose = True)
+            curvedsky.alm2map(alm, self.omap, spin = [0, 2], verbose = True)
+        elif self.nside is not None:
+            self.omap = hp.alm2map(alm, self.nside)
         else:
-            self.omap = hp.alm2map(self.alm, self.nside)
-        
+            raise ValueError("You must specify either nside or both of shape and wcs")
 
-    def signal(self, nu, **kwargs):
+    def signal(self, nu=None, modulation=True, output_units="uK_RJ", **kwargs):
         """Return map in uK_RJ at given frequency or array of frequencies"""
 
+        # If nothing is specified, we default to providing an unmodulated map
+        # at 148 GHz. The value 148 Ghz does not matter if the output is in
+        # uK.
+        if not(modulation) and nu is None: nu = [148.]
         try:
             nnu = len(nu)
         except TypeError:
             nnu = 1
             nu = np.array([nu])
         
+        # use tile to output the same map for all frequencies
         out = np.tile(self.omap, (nnu, 1, 1))
-        out *= pysm.convert_units(self.input_units, "uK_RJ", nu).reshape((nnu, 1, 1))
+        if self.wcs is not None: out = enmap.enmap(out,self.wcs)
+        out *= pysm.convert_units(self.input_units, output_units, nu).reshape((nnu, 1, 1))
+
+        if modulation:
+            # TODO: implement frequency dependent modulation
+            pass
 
         # the output of out is always 3D, (num_freqs, IQU, npix), if num_freqs is one
         # we return only a 2D array.
