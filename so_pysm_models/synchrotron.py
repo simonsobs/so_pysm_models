bd39c14d1bbc48d7330fec6c22869707ac36c0c0
Andrea Zonca <code@andreazonca.com>
2019-02-05 13:22:35 -0800

    fix filters and move to other module

--- so_pysm_models/synchrotron.py
+++ so_pysm_models/synchrotron.py
@@ -5,31 +5,7 @@ import healpy as hp
 import math
 
 from . import laws
-
-
-def create_high_pass_filter(l1, l2, lmax):
-    ell = np.arange(l1, l2)
-    wl = np.zeros(len(ell))
-    wl[np.where(ell >= l2)] = 1.0
-    wl[ell] = 0.5 * (1 - np.cos(np.pi * (ell - l1) / (l2 - l1)))
-
-
-def create_low_pass_filter(l1, l2, lmax):
-    ell = np.arange(l1, l2)
-    wl = np.zeros(len(ell))
-    wl[np.where(ell <= l1)] = 1.0
-    wl[ell] = 0.5 * (1 - np.cos(np.pi * (l2 - ell) / (l2 - l1)))
-    return wl
-
-
-def apply_filter(hmap, filt):
-    nside = hp.get_nside(hmap)
-    lmax = 3 * nside
-    filt = filt[0:lmax]
-    alm = hp.map2alm(hmap, lmax=lmax)
-    almf = hp.almxfl(alm, filt)
-    hmap_out = hp.alm2map(almf, nside)
-    return hmap_out
+from . import filter_utils
 
 
 class GaussianSynchrotron:
@@ -173,10 +149,10 @@ class GaussianSynchrotron:
             amp_sync[0] += self.Toffset
             lbreak_TT += 1
         if self.target_nside > 64:
-            lpf = create_low_pass_filter(l1=30, l2=60, lmax=64 * 3 - 1)
-            hpf = create_high_pass_filter(l1=30, l2=60, lmax=nell - 1)
-            clTT_sync_hpf = clTT_sync * hpf
-            amp_sync[0] = apply_filter(amp_sync[0], lpf)
+            low_pass_filter = filter_utils.create_low_pass_filter(l1=30, l2=60, lmax=64 * 3 - 1)
+            high_pass_filter = filter_utils.create_high_pass_filter(l1=30, l2=60, lmax=nell - 1)
+            clTT_sync_hpf = clTT_sync * high_pass_filter
+            amp_sync[0] = filter_utils.apply_filter(amp_sync[0], low_pass_filter)
             np.random.seed(mseed)
             amp_sync_hell = np.array(
                 hp.synfast(
