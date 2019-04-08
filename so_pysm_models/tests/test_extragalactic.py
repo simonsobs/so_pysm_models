ff2402b622af5fa3a5b37d9aed51b186c0c5f87d
Andrea Zonca <code@andreazonca.com>
2019-04-08 11:35:05 -0700

    implemented unit tests for ksz and tsz

--- so_pysm_models/tests/test_extragalactic.py
+++ so_pysm_models/tests/test_extragalactic.py
@@ -2,7 +2,7 @@ import numpy as np
 import healpy as hp
 
 from .. import utils
-from .. import WebSkyCIB
+from .. import WebSkyCIB, WebSkySZ
 
 
 def test_cib(tmp_path, monkeypatch):
@@ -21,4 +21,40 @@ def test_cib(tmp_path, monkeypatch):
     interpolated_map = interp.signal(nu=97)
     np.testing.assert_allclose(
         np.interp(97, [94, 100], [0, 1]) * np.ones((1, shape)), interpolated_map
+    )
+
+def test_ksz(tmp_path, monkeypatch):
+
+    monkeypatch.setattr(utils, "PREDEFINED_DATA_FOLDERS", [str(tmp_path)])
+    nside = 4
+    shape = hp.nside2npix(nside)
+
+    path = tmp_path / "websky" / "0.3"
+    path.mkdir(parents=True)
+    hp.write_map(path / "ksz.fits", np.ones(shape, dtype=np.float32))
+    hp.write_map(path / "cib_0100.fits", np.ones(shape, dtype=np.float32))
+
+    ksz = WebSkySZ("0.3", sz_type="kinetic", target_nside=nside)
+
+    ksz_map = ksz.signal(nu=100)
+    np.testing.assert_allclose(
+        np.ones(ksz_map.shape)*0.777228, ksz_map, rtol=1e-4
+    )
+
+def test_tsz(tmp_path, monkeypatch):
+
+    monkeypatch.setattr(utils, "PREDEFINED_DATA_FOLDERS", [str(tmp_path)])
+    nside = 4
+    shape = hp.nside2npix(nside)
+
+    path = tmp_path / "websky" / "0.3"
+    path.mkdir(parents=True)
+    hp.write_map(path / "tsz.fits", np.ones(shape, dtype=np.float32)*1e-6)
+    hp.write_map(path / "cib_0100.fits", np.ones(shape, dtype=np.float32))
+
+    tsz = WebSkySZ("0.3", sz_type="thermal", target_nside=nside)
+
+    tsz_map = tsz.signal(nu=100)
+    np.testing.assert_allclose(
+        np.ones(tsz_map.shape)*-3.193671, tsz_map, rtol=1e-4
     )
