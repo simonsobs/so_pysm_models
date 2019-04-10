afaa8a9a0c67ec9fad75e6f71199452f59499667
Andrea Zonca <code@andreazonca.com>
2019-04-10 16:39:15 -0700

    [bugfix] fix name of ame

--- so_pysm_models/so_models.py
+++ so_pysm_models/so_models.py
@@ -198,7 +198,7 @@ def SO_a0(nside, pixel_indices=None, mpi_comm=None, nside_template=512):
     ]
 
 def SO_a1(nside, pixel_indices=None, mpi_comm=None, nside_template=512):
-    T_map1 = get_data_from_url("ame_T_ns{}.fits".format(nside_template))
+    T_map1 = get_data_from_url("ame1_T_ns{}.fits".format(nside_template))
     T_map2 = get_data_from_url("ame2_T_ns{}.fits".format(nside_template))
     Q_dust = get_data_from_url("dust_Q_ns{}.fits".format(nside_template))
     U_dust = get_data_from_url("dust_U_ns{}.fits".format(nside_template))
