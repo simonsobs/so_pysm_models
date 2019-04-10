71e66889864ed3300fc4f99a8f170a3f3d845ba0
Andrea Zonca <code@andreazonca.com>
2019-04-10 16:36:06 -0700

    test all components

--- so_pysm_models/tests/test_so_models.py
+++ so_pysm_models/tests/test_so_models.py
@@ -1,15 +1,20 @@
 import numpy as np
 
 from .. import get_so_models
+import pytest
+
+components_dict = {comp[0]: comp for comp in ["dust", "synchrotron", "freefree", "ame"]}
 
 
-def test_get_so_models():
+@pytest.mark.parametrize("model_tag", ["SO_d0", "SO_s0", "SO_f0", "SO_a0"])
+def test_get_so_models(model_tag):
     """Test the `get_so_models` function
 
     Only check that we can access the 512 version of the template
     and that the result has no NaN"""
     from pysm import Sky
-    sky = Sky({
-            "dust" : get_so_models("SO_d0", nside=128),
-    })
-    assert not np.any(np.isnan(sky.dust(nu=100)))
+
+    component_name = components_dict[model_tag.split("_")[1][0]]
+    sky = Sky({component_name: get_so_models(model_tag, nside=128)})
+    component = getattr(sky, component_name)
+    assert not np.any(np.isnan(component(nu=100)))
