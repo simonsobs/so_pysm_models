c96023f7a37367ab46439f3729d5b6b0003db82c
Andrea Zonca <code@andreazonca.com>
2019-05-10 13:37:42 -0700

    use same class creation for same component

--- so_pysm_models/so_models.py
+++ so_pysm_models/so_models.py
@@ -35,15 +35,29 @@ def get_so_models(key, nside, pixel_indices=None, mpi_comm=None):
     else:
         nside_template = 512
 
-    if key == "SO_d0":
+    if key.startswith("SO_d"):
+        if key == "SO_d0":
+            map_mbb_index = 1.53
+            map_mbb_temperature = 19.6 * u.K
+        elif key == "SO_d1":
+            map_mbb_index = get_data_from_url(
+                "variable_spectral_index/beta_dust_ns{}_1deg.fits".format(
+                    nside_template
+                )
+            )
+            map_mbb_temperature = get_data_from_url(
+                "variable_spectral_index/temperature_dust_ns{}_1deg.fits".format(
+                    nside_template
+                )
+            )
         model = pysm.ModifiedBlackBody(
             get_data_from_url("dust_T_ns{}.fits".format(nside_template)),
             get_data_from_url("dust_Q_ns{}.fits".format(nside_template)),
             get_data_from_url("dust_U_ns{}.fits".format(nside_template)),
             freq_ref_I=545 * u.GHz,
             freq_ref_P=353 * u.GHz,
-            map_mbb_index=1.53,
-            map_mbb_temperature=19.6 * u.K,
+            map_mbb_index=map_mbb_index,
+            map_mbb_temperature=map_mbb_temperature,
             nside=nside,
             has_polarization=True,
             unit_I=u.uK_RJ,
@@ -53,32 +67,22 @@ def get_so_models(key, nside, pixel_indices=None, mpi_comm=None):
             pixel_indices=pixel_indices,
             mpi_comm=mpi_comm,
         )
-    elif key == "SO_d1":
-        model = pysm.ModifiedBlackBody(
-            get_data_from_url("dust_T_ns{}.fits".format(nside_template)),
-            get_data_from_url("dust_Q_ns{}.fits".format(nside_template)),
-            get_data_from_url("dust_U_ns{}.fits".format(nside_template)),
-            freq_ref_I=545 * u.GHz,
-            freq_ref_P=353 * u.GHz,
-            map_mbb_index=get_data_from_url("variable_spectral_index/beta_dust_ns{}_1deg.fits".format(nside_template)),
-            map_mbb_temperature=get_data_from_url("variable_spectral_index/temperature_dust_ns{}_1deg.fits".format(nside_template)),
-            nside=nside,
-            has_polarization=True,
-            unit_I=u.uK_RJ,
-            unit_Q=u.uK_RJ,
-            unit_U=u.uK_RJ,
-            unit_mbb_temperature=u.K,
-            pixel_indices=pixel_indices,
-            mpi_comm=mpi_comm,
-        )
-    elif key == "SO_s0":
+    elif key.startswith("SO_s"):
+        if key == "SO_s0":
+            index = -3.1
+        elif key == "SO_s1":
+            index = get_data_from_url(
+                "variable_spectral_index/beta_synch_ns{}_1deg.fits".format(
+                    nside_template
+                )
+            )
         model = pysm.PowerLaw(
             map_I=get_data_from_url("synch_T_ns{}.fits".format(nside_template)),
             map_Q=get_data_from_url("synch_Q_ns{}.fits".format(nside_template)),
             map_U=get_data_from_url("synch_U_ns{}.fits".format(nside_template)),
             freq_ref_I=408 * u.MHz,
             freq_ref_P=23 * u.GHz,
-            map_pl_index=-3.1,
+            map_pl_index=index,
             nside=nside,
             unit_I=u.uK_RJ,
             unit_Q=u.uK_RJ,
