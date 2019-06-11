eac58f7326db5810ad59134879616d0383d207a1
Andrea Zonca <code@andreazonca.com>
2019-06-10 18:10:05 -0700

    rotate maps utility script

---
+++ so_pysm_models/utils/rotate_maps.py
@@ -0,0 +1,18 @@
+import healpy as hp
+import sys
+from pathlib import Path
+
+folder = Path(sys.argv[1])
+
+out_folder = folder / "equatorial"
+
+rot = hp.Rotator(coord=["G","C"])
+
+for f in folder.glob("*.fits"):
+    print(f)
+    f = str(f)
+    m = hp.read_map(f)
+    m_equatorial = rot.rotate_map_alms(m)
+    out_file = f.replace(str(folder), str(out_folder))
+    print(out_file)
+    hp.write_map(out_file, m_equatorial)
