423c9cdbbe7e25d173b5dd276c8863646a4b2ac9
Andrea Zonca <code@andreazonca.com>
2020-10-06 17:48:56 -0700

    test clipping

--- so_pysm_models/tests/test_cmb.py
+++ so_pysm_models/tests/test_cmb.py
@@ -27,3 +27,21 @@ def test_precomputed_alms():
     expected_map = hp.read_map(save_name, field=(0, 1, 2)) << u.uK_CMB
     assert simulated_map.shape[0] == 3
     assert_quantity_allclose(simulated_map, expected_map)
+
+def test_precomputed_alms_clip():
+    # If clipping to lmax of 95 were not applied, this test would fail
+    alms_filename = get_pkg_data_filename(
+        "data/Planck_bestfit_alm_seed_583_lmax_120_K_CMB.fits.zip"
+    )
+    save_name = get_pkg_data_filename("data/map_nside_32_from_Planck_bestfit_alm_seed_583_K_CMB.fits.zip")
+    nside = 32
+    # Make an IQU sim
+    precomputed_alms = PrecomputedAlms(
+        alms_filename, nside=nside, input_units="uK_CMB",
+        has_polarization=True,
+        #input_reference_frequency=148*u.GHz
+    )
+    simulated_map = precomputed_alms.get_emission(148*u.GHz).to(u.uK_CMB, equivalencies=u.cmb_equivalencies(148*u.GHz))
+    expected_map = hp.read_map(save_name, field=(0, 1, 2)) << u.uK_CMB
+    assert simulated_map.shape[0] == 3
+    assert_quantity_allclose(simulated_map, expected_map)
